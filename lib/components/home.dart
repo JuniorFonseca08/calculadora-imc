feba8018f48dc5301d153b3201b994c75be2ddab
JuniorFonseca08 <JuniorFonseca08>
2023-07-21 16:32:25 -0300

    botão

--- lib/components/home.dart
+++ lib/components/home.dart
@@ -7,40 +7,40 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {  
-
+class _HomeState extends State<Home> {
   GlobalKey<FormState> _formKey = GlobalKey();
   final pesoController = TextEditingController();
   final alturaController = TextEditingController();
   String _inforText = 'Informe seus dados.';
 
-  void _reset(){
+  void _reset() {
     pesoController.text = '';
     alturaController.text = '';
-   setState(() {
-    _inforText = 'Informe seus dados.';
-    _formKey = GlobalKey<FormState>();
-   });
+    setState(() {
+      _inforText = 'Informe seus dados.';
+      _formKey = GlobalKey<FormState>();
+    });
   }
 
-  void _calcular(){
+  void _calcular() {
     setState(() {
-    double peso = double.parse(pesoController.text);
-    double altura = double.parse(alturaController.text) / 100;
-    double imc = peso / (altura * altura);
-    if (imc < 18.6) {
-      _inforText = 'Abaixo do peso (IMC: ${imc.toStringAsPrecision(3)})';
-    }else if(imc >= 18.6 && imc < 24.9){
-      _inforText = 'Peso Ideal (IMC: ${imc.toStringAsPrecision(3)})';
-    }else if(imc >= 24.9 && imc < 29.9){
-      _inforText = 'Levemente acima do peso (IMC: ${imc.toStringAsPrecision(3)})';
-    }else if(imc >= 29.9 && imc < 34.9){
-      _inforText = 'Obesidade Grau I (IMC: ${imc.toStringAsPrecision(3)})';
-    }else if(imc >= 34.9 && imc < 39.9){
-      _inforText = 'Obesidade Grau II (IMC: ${imc.toStringAsPrecision(3)})';
-    }else if(imc > 40){
-      _inforText = 'Obesidade Grau III (IMC: ${imc.toStringAsPrecision(3)})';
-    }
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text) / 100;
+      double imc = peso / (altura * altura);
+      if (imc < 18.6) {
+        _inforText = 'Abaixo do peso (IMC: ${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _inforText = 'Peso Ideal (IMC: ${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _inforText =
+            'Levemente acima do peso (IMC: ${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _inforText = 'Obesidade Grau I (IMC: ${imc.toStringAsPrecision(3)})';
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _inforText = 'Obesidade Grau II (IMC: ${imc.toStringAsPrecision(3)})';
+      } else if (imc > 40) {
+        _inforText = 'Obesidade Grau III (IMC: ${imc.toStringAsPrecision(3)})';
+      }
     });
   }
 
@@ -51,73 +51,88 @@ class _HomeState extends State<Home> {
       child: Form(
         key: _formKey,
         child: Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        const Icon(
-          Icons.perm_identity_sharp,
-          size: 120,
-          color: Color.fromARGB(255, 218, 164, 16),
-        ),
-        TextFormField(
-          keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-              labelText: 'Peso (kg)',
-              labelStyle: TextStyle(color: Color.fromARGB(255, 218, 164, 16))),
-          textAlign: TextAlign.center,
-          style:
-              TextStyle(fontSize: 25, color: Color.fromARGB(255, 218, 164, 16)),
-          controller: pesoController,
-          validator: (value) {
-            if (value!.isEmpty) {
-              return 'Insira seu peso';
-            }
-          },
-        ),
-        TextFormField(
-          keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-              labelText: 'Altura (cm)',
-              labelStyle: TextStyle(color: Color.fromARGB(255, 218, 164, 16))),
-          textAlign: TextAlign.center,
-          style:
-              TextStyle(fontSize: 25, color: Color.fromARGB(255, 218, 164, 16)),
-              controller: alturaController,
+          //crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Icon(
+              Icons.perm_identity_sharp,
+              size: 120,
+              color: Color.fromARGB(255, 218, 164, 16),
+            ),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                  labelText: 'Peso (kg)',
+                  labelStyle:
+                      TextStyle(color: Color.fromARGB(255, 218, 164, 16))),
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  fontSize: 25, color: Color.fromARGB(255, 218, 164, 16)),
+              controller: pesoController,
               validator: (value) {
-            if (value!.isEmpty) {
-              return 'Insira uma altura';
-            }
-          }
+                if (value!.isEmpty) {
+                  return 'Insira seu peso';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                    labelText: 'Altura (cm)',
+                    labelStyle:
+                        TextStyle(color: Color.fromARGB(255, 218, 164, 16))),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontSize: 25, color: Color.fromARGB(255, 218, 164, 16)),
+                controller: alturaController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Insira uma altura';
+                  }
+                  return null;
+                }),
+            const SizedBox(height: 16),
+            //SizedBox(
+              //height: 50,
+              //child: 
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _calcular();
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 218, 164, 16),
+                  fixedSize: const Size(double.maxFinite, 50),
+                  shape: const StadiumBorder()
+                ),
+                child: const Text(
+                  'Calcular',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            //),
+            const SizedBox(height: 16),
+            Text(
+              _inforText,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  fontSize: 25, color: Color.fromARGB(255, 218, 164, 16)),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton.icon(
+              onPressed: _reset,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 218, 164, 16),
+                fixedSize: const Size(100, 50),
+                shape: StadiumBorder()
+              ),
+              icon: const Icon(Icons.refresh_rounded),
+              label: const Text('Reset'),
+              
+            ),
+          ],
         ),
-        const SizedBox(height: 16),
-        Container(height: 50,
-          child: ElevatedButton(onPressed: (){
-            if (_formKey.currentState!.validate()) {
-              _calcular();
-            }
-          }, 
-          child: const Text('Calcular', style: TextStyle(fontSize: 25),),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: const Color.fromARGB(255, 218, 164, 16),),),
-        ),
-        const SizedBox(height: 16),
-        Text(_inforText,
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 25, color: Color.fromARGB(255, 218, 164, 16)),
-        ),
-        const SizedBox(height: 50),
-        Container(
-  child: ElevatedButton.icon(
-    onPressed: _reset,
-    style: ElevatedButton.styleFrom(
-      backgroundColor: const Color.fromARGB(255, 218, 164, 16),
-      fixedSize: Size(60, 50),
-    ),
-    icon: Icon(Icons.refresh_rounded),
-    label: Text('Reset'),
-  ),
-),
-      ], 
-    ),
       ),
     );
   }
